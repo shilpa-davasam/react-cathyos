776d761125d04e28f21cf1ea80394b66b48739d7
shilpa-davasam <shilpadg18@gmail.com>
2019-10-16 14:05:15 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
-import axios from 'axios';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 //import components
-import AddToCart from './Components/AddToCart';
-import Orders from './Components/Orders';
+import AddToCart from "./Components/AddToCart";
+import Orders from "./Components/Orders";
 
-import './style.css';
+import "./style.css";
 
 class App extends Component {
   constructor() {
@@ -17,67 +17,87 @@ class App extends Component {
       isLoaded: false,
       error: "",
       orders: [],
-	  addedItem: ""
+      addedItem: ""
     };
   }
 
-  componentDidMount(){
-      axios.get("https://api.myjson.com/bins/qhnfp")
-        .then(res => this.setState({items : res.data, isLoaded: true}))
-        .catch(err => this.setState({isLoaded: true, error: err}));
+  componentDidMount() {
+    axios
+      .get("https://api.myjson.com/bins/qhnfp")
+      .then(res => this.setState({ items: res.data, isLoaded: true }))
+      .catch(err => this.setState({ isLoaded: true, error: err }));
   }
 
-  //update orders in cart 
-  updateOrders = (obj, op="add") => {
+  //update orders in cart
+  updateOrders = (obj, op = "add") => {
     const orders = [...this.state.orders];
     const order = orders.find(order => order.id === obj.id);
-    if(order){
-		switch(op){
-			case "add": order.quantity = order.quantity + 1;
-				break;
-			case "sub" : 
-				if(order.quantity > 1){
-					order.quantity = order.quantity - 1;
-				}
-					
-				break;
-		}
-    }
-    else{
+    if (order) {
+      switch (op) {
+        case "add":
+          order.quantity = order.quantity + 1;
+          break;
+        case "sub":
+          if (order.quantity > 1) {
+            order.quantity = order.quantity - 1;
+          }
+        
+          break;
+      }
+    } else {
       orders.push(obj);
     }
-    this.setState({orders, addedItem: obj.name});
-    console.log(this.state.orders);
-  }
-  removeOrder = (obj) => {
-	  const orders = [...this.state.orders];
-	  const index = orders.findIndex(order => order.id === obj.id);
-	  orders.splice(index, 1);
-	  this.setState({orders});
-  }
+    this.setState({ orders, addedItem: obj.name });
+    
+  };
+  removeOrder = obj => {
+    const orders = [...this.state.orders];
+    const index = orders.findIndex(order => order.id === obj.id);
+    orders.splice(index, 1);
+    this.setState({ orders });
+  };
 
   render() {
-    const { state: {items, isLoaded, error, orders, addedItem}, updateOrders, removeOrder} = this;
-    if(!isLoaded){
+    const {
+      state: { items, isLoaded, error, orders, addedItem },
+      updateOrders,
+      removeOrder
+    } = this;
+    if (!isLoaded) {
       return <div>Loading...</div>;
-    }
-    else if(error){
+    } else if (error) {
       return <div>{error}</div>;
-    }
-    else{
+    } else {
       return (
-		<Router>
-			<Switch>
-				<Route exact path="/" render={() =>  <AddToCart items={items} updateOrders={updateOrders} addedItem={addedItem} orders={orders}/>}/>
-				<Route path="/orders" render={() => <Orders orders={orders} updateOrders={updateOrders} 
-						removeOrder={removeOrder}/>} />
-			</Switch>
-		</Router>
-            
-          );
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <AddToCart
+                  items={items}
+                  updateOrders={updateOrders}
+                  addedItem={addedItem}
+                  orders={orders}
+                />
+              )}
+            />
+            <Route
+              path="/orders"
+              render={() => (
+                <Orders
+                  orders={orders}
+                  updateOrders={updateOrders}
+                  removeOrder={removeOrder}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      );
     }
-   
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
